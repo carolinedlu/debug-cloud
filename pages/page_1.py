81f349869067178fec15b718beb775f833d8084d
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-04-27 13:47:55 -0400

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -9,5 +9,5 @@ from streamlit_javascript import st_javascript
 import streamlit as st
 
 url = st_javascript("await fetch('').then(r => window.parent.location.href)")
-
-st.write(url)
+st.write(url.rsplit('/',1)[1])
+# st.write(url)
