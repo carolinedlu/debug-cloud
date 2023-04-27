b46b8a39ac91e06881e461f273f776d57534dfb8
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-04-27 13:41:47 -0400

    Update page_1.py

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,6 +1,13 @@
-import streamlit as st
-import sys
-import os
+# import streamlit as st
+# import sys
+# import os
 
-st.write(sys.argv[0])
-st.write(os.path.basename(sys.argv[0]))
+# st.write(sys.argv[0])
+# st.write(os.path.basename(sys.argv[0]))
+
+from streamlit_javascript import st_javascript
+import streamlit as st
+
+url = st_javascript("await fetch('').then(r => window.parent.location.href)")
+
+st.write(url)
