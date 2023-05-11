180256c7aac467474a090caca7ed3dd5ff0a94d4
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-05-10 21:09:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import os
-import sys
+# import os
+# import sys
 
-st.write(sys.argv[0])
+# st.write(sys.argv[0])
 
-cwd = os.getcwd()
-st.write(cwd)
+# cwd = os.getcwd()
+# st.write(cwd)
+
+st.write.
